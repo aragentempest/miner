ce04e7d69e0860535bf0a2238efaaf7872f47822
aragentempest <99246419+aragentempest@users.noreply.github.com>
2022-02-08 16:35:40 +0800

    Create veruscoin_bitbucket.sh

---
+++ veruscoin_bitbucket.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RApLikC5uZwV6fiZB1pQ3Yc4njJTAVB3rU.aragen -p x --cpu 4
